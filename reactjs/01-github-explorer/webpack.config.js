da212517ee5d57c6ab1895eba098f6130842a62a
gdjesus <gabriel.jesus@performait.com>
2021-04-04 02:36:43 -0300

    Update estudos

--- reactjs/01-github-explorer/webpack.config.js
+++ reactjs/01-github-explorer/webpack.config.js
@@ -1,20 +1,48 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.jsx'), // path.resolve coloca o caminho correto independente de sistema operacional, aqui é referenciado o arquivo inicio da aplicação
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+    entry: path.resolve(__dirname, 'src', 'index.tsx'), // path.resolve coloca o caminho correto independente de sistema operacional, aqui é referenciado o arquivo inicio da aplicação
     output: {
         path: path.resolve(__dirname, 'dist'), // caminho do arquivo de saída da aplicação
         filename: 'bundle.js' // Nome do arquivo de saída
     },
     resolve: {
-        extensions: ['.js', '.jsx'] // Quais extensões serão interpretadas
+        extensions: ['.js', '.jsx', '.ts', '.tsx'] // Quais extensões serão interpretadas
     },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        hot: true
+    },
+    plugins: [
+      isDevelopment && new ReactRefreshWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ].filter(Boolean), // Filtrando porque isDevelpoment quando for produção será false, e isto não é um plugin do webpack, resultando em erro.
     module: {
         rules: [
             {
-                test: /\.jsx$/, // RegExp para verificar se o arquivo termina da forma correta, $ significa terminar, ponto por padrão é qualquer caractere, entao pra validar ponto mesmo seria \.
+                test: /\.(j|t)sx$/, // RegExp para verificar se o arquivo termina da forma correta, $ significa terminar, ponto por padrão é qualquer caractere, entao pra validar ponto mesmo seria \. (j|t) diz que pode ser um ou outro
                 exclude: /node_modules/, // Desconsiderar tudo que estiver no node_modules
-                use: 'babel-loader' // Integração entre babel e webpack
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    plugins: [
+                      isDevelopment && require.resolve('react-refresh/babel')
+                    ].filter(Boolean)
+                  }
+                } // Integração entre babel e webpack
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     }
